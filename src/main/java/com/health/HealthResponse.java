c0630dff4890e36dd2af3b38ab3c13479773fa10
busterz3682 <87509072+busterz3682@users.noreply.github.com>
2026-07-24 08:19:38 +0000

    feat: 프로젝트 시작

---
+++ src/main/java/com/health/HealthResponse.java
@@ -0,0 +1,4 @@
+package com.taeyn.smalltalk.health;
+
+public record HealthResponse(String status) {
+}
---
+++ src/main/java/com/taeyn/smalltalk/SmalltalkApplication.java
@@ -0,0 +1,13 @@
+package com.taeyn.smalltalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmalltalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmalltalkApplication.class, args);
+	}
+
+}
